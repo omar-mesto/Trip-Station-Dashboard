c43be01768502aa4f7429752e33f432f6f044a40
omar-mesto <omisto014@gmail.com>
2025-08-23 05:27:16 +0300

    Admin Logout

--- queries/auth/admin.ts
+++ queries/auth/admin.ts
@@ -1,6 +1,10 @@
-  import { useAPI } from '@@/services/baseApi'
+import { useAPI } from '@@/services/baseApi'
 import type { adminModel } from '~/models/adminModel'
 
 export const useLoginAdmin = (user: adminModel) => {
-    return useAPI({ url: '/auth/login', payload: user, queryKey: 'admin', type: 'POST' })
+  return useAPI({ url: '/auth/login', payload: user, queryKey: 'admin', type: 'POST' })
+}
+
+export const useLogout = () => {
+  return useAPI({ url: '/auth/logout', queryKey: 'logout', type: 'POST' })
 }
